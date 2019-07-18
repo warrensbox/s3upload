e0b91890ccf28439c338cd7a32442dfc3bc6a413
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2019-07-18 13:26:14 -0500

    exclude file

--- lib/core_test.go
+++ lib/core_test.go
@@ -49,7 +49,7 @@ func TestConfiguration(t *testing.T) {
 		t.Errorf("Configuration file not found: %s [unexpected]\n", constructor.ConfigFile)
 	}
 
-	if constructor.Exclude == "main,README.md" {
+	if constructor.Exclude == "main,README.md,s3upload" {
 		t.Logf("Excludes found: %s [expected]\n", constructor.ConfigFile)
 	} else {
 		t.Errorf("Excludes not found: %s [unexpected]\n", constructor.ConfigFile)
